688fd905ae6f5da3be3678fec91651f96a59fec5
Pete Birkinshaw <pete@binary-ape.org>
2010-01-10 18:01:31 +0000

    Basic chooser working, start of error catching work. All rather messy still.

--- shib_simulator.rb
+++ shib_simulator.rb
@@ -2,6 +2,7 @@ class ShibSimulator
   
   require 'haml'
   require 'haml/template'
+  require 'yaml'
   
  
  
@@ -13,9 +14,20 @@ class ShibSimulator
   ## Load default data
   DEFAULT_MAPPER = {}
   #DEFAULT_DATA = prepare_user_data()
+  CONTENT_TYPE = { "Content-Type" => "text/html; charset=utf-8" }
 
-   @@views = nil
- 
+  ## Initial vars for storing cached data
+  @@views    = nil
+  @@users    = nil
+  @@orgtree  = nil 
+  
+  ## Default configuration
+  @@config = {
+    :org_from          => :attribute,
+    :org_attr          => 'organisation',
+    :org_mapper_config => :default  
+  }
+  
   def initialize(app, data_source=DEFAULT_DATA, data_mapper=DEFAULT_MAPPER)
     
     @app = app
@@ -29,42 +41,75 @@ class ShibSimulator
     # ...
 
   end
-
+  
+  ## Selecting an action and returning to the Rack stack 
   def call(env)
     
     ## Peek at user input, they might be talking to us
     req = Rack::Request.new(env)
     
-    ## Reset session?
-    reset_session(env) if req.params['shibsim_reset']
+    begin
+      
+      ## Reset session?
+      reset_session(env) if req.params['shibsim_reset']
   
-    ## Already set? Then our work here is done.
-    return @app.call(env) if env["rack.session"]['shib_simulator_active']
+      ## Already set? Then our work here is done. #### <-- Need to ReInject!
+      return @app.call(env) if env["rack.session"]['shib_simulator_active']
 
-    ## Directly requested user or user? (via URL param)
-    user_id = req.params['shibsim_user']
+      ## Directly requested user or user? (via URL param)
+      user_id = req.params['shibsim_user']
 
-    ## Bodge session or display a page showing the list of available fixtures
-    if user_id 
+      ## Bodge session or display a page showing the list of available fixtures
+      if user_id 
       
-      set_session(env, user_id)
-      tidy_request(env)
-
-      return @app.call(env)
+        ## Get our user information using the param
+        user_details = users[user_id]
+      
+        ## A crude check that we've really found some attributes...
+        if user_details and user_details.kind_of(Hash) and
+          user_details.size > 1 
           
-    else
- 
-      page_body = render_page(:user_chooser)
+          ## Update session
+          set_session(env, user_id)
+          
+          ## Add headers to request
+          # ...
+          
+          ## Clean up
+          tidy_request(env)
 
-      status, headers, body = @app.call env
+          return @app.call(env)
+          
+        end
       
-      tidy_request(env)
+        ## User was request but no user details were found
+        message = "User with ID '#{user_id}' could not be found!" 
+
+        tidy_request(env)
         
-      return 200, { "Content-Type" => "text/html; charset=utf-8" }, [page_body.to_s] 
+        return user_chooser_action(env, { :message => message, :code => 401 })
+        
+      else
+         
+         ## No user requested, so show the chooser
+         tidy_request(env)
+         return user_chooser_action(env)
       
+      end
+
+    rescue => exception
+    
+      return fatal_error_action(env, exception)
+    
     end
 
   end
+  
+  ##
+  ##
+  ##
+  
+  private
   
   ## Wipe clean the Rack session info for this middleware
   def reset_session(env)
@@ -74,24 +119,62 @@ class ShibSimulator
     tidy_request(env)
     
   end
+
+  ## Error page for unrecoverable situations
+  def fatal_error_action(env, exception)
+    
+    render_locals = { :message => exception.to_s }
+    page_body = render_page(:fatal_error, render_locals)
+
+    status, headers, body = @app.call env
+
+    return 500, CONTENT_TYPE, [page_body.to_s]
+    
+  end
+  
+  ## Controller for user presentation page
+  def user_chooser_action(env, options={}) 
+    
+     message = options[:message] 
+     code    = options[:code] || 200
+    
+     render_locals = { :organisations => organisations, :users => users, :message => message }
+     page_body = render_page(:user_chooser, render_locals)
+
+     status, headers, body = @app.call env
+       
+     return code, CONTENT_TYPE, [page_body.to_s]
+    
+  end
   
   ## Display a chooser page
-  def render_page(view, options={})
+  def render_page(view, locals={})
     
     ## HAML rendering options
     Haml::Template.options[:format] = :html5
     
     ## Render and return the page
     haml = Haml::Engine.new(views[view])
-    return haml.render
+    return haml.render(Object.new, locals)
+    
+  end
+  
+  ## Add information to the headers passed to application
+  def inject_headers(env, user_details)
+    
+    
+    
+  end
+  
+  ## Convert user data into Shibboleth SP style header values
+  def map_attributes_to_headers
+    
+    
     
   end
   
   ## 
   def set_session(env, user_id)
-  
-    ## Get our user information
-    # ...
     
     ## Convert to proper format that matches the live SP                                                                                                                                         
     # ...
@@ -120,8 +203,20 @@ class ShibSimulator
     
   end
   
-  private
+  ## List user records by ID
+  def users
   
+    return user_data[0]
+  
+  end
+  
+  ## List user records by ID
+  def organisations
+  
+    return user_data[1]
+  
+  end
+
   ## Load and prepare HAML views
   def views
     
@@ -142,13 +237,38 @@ class ShibSimulator
     
   end
   
-  ## Load and prepare user fixture data or default data 
-  def prepare_user_data
+  ## Provide user data for chooser and header injection
+  def user_data
     
+    unless @@users && @@orgtree
     
+      @@users   = Array.new
+      @@orgtree = Hash.new 
+      
+      user_fixture_file_location = "#{File.dirname(__FILE__)}/default_data/user_data.yml"
+      
+      fixture_data = YAML.load_file(user_fixture_file_location)
     
+      fixture_data.each_pair do |label, record| 
+       
+        record['shibsim_label'] = label
+      
+        rid  = record['id']
+        rorg = record['organisation']
+
+        @@users[rid]    =   record
+        @@orgtree[rorg] ||= Array.new
+        @@orgtree[rorg] <<  record 
+       
+      end
+    
+    end
+    
+    return [@@users, @@orgtree]
     
   end
+  
+
   
 end
 
