663669bec9120c440c10b32c951c5b3828864643
Pete Birkinshaw <pete@binary-ape.org>
2010-03-14 23:09:23 +0000

    Version bump to 0.2.0

---
+++ lib/default_config/shibsim_filter.rb
@@ -0,0 +1,16 @@
+####
+
+module Rack
+  module ShibSimulatorFilter
+    
+    ## Process a user's record to make resemble Shibboleth SP attributes 
+    def user_record_filter(user_record)
+      
+      
+      
+      return user_record    
+      
+    end
+    
+  end
+end
