c25631b7340964d39dff17f8ebaa492fbf04e7c4
Pete Birkinshaw <pete@binary-ape.org>
2010-09-26 22:18:47 +0100

    Doing a bad broken local commit halfway through big refactoring of Shibkit::Simulator

---
+++ shibkit/rack/debug.rb
@@ -0,0 +1,41 @@
+class Shibkit::Rack::Debug
+
+  require 'deep_merge'
+
+  require 'shibkit/sp_assertion'
+  
+      
+  ## Easy access to Shibkit's configuration settings
+  include Shibkit::Configured
+  
+  
+  def initialize(app)
+    
+    #@app = app
+    
+    ## Cache a couple of options (config should be frozen anyway)
+    #@user_id_name   = config.shim_user_id_name
+    #@assertion_name = config.shim_sp_assertion_name
+    
+  end
+  
+  ## Middleware entry point: Extract info from SP headers, encapsulate in user object, continue
+  def call(env)
+    
+    ## Extract information from SP headers and pass to user assertion object
+    #sp_assertion_data = process_sp_session(env)
+    #shib_session = Shibkit::SPAssertion.new(sp_assertion_data)
+    
+    ## Check that user is consistent with SP status, to cope with changes:
+    #
+    
+    ## Store in session # TODO: Make this configurable
+    #env['rack.session'][@assertion_name] = shib_session  
+    
+    return @app.call(env)
+    
+  end
+
+  private
+  
+end
--- shibkit/rack/simulator.rb
+++ shibkit/rack/simulator.rb
@@ -1,52 +1,45 @@
 ##
 ##
+
+require 'uuid'
+require 'haml'
+require 'yaml'
+require 'time'
+require 'rack/logger'
+
+## Require various mixins too
+require 'shibkit/rack/simulator/mixins/render'
+require 'shibkit/rack/simulator/mixins/actions'
+require 'shibkit/rack/simulator/mixins/injection'
+require 'shibkit/rack/simulator/mixins/logging'
+
+## Default record filter mixin code
+require 'shibkit/rack/simulator/record_filter'
+
 module Shibkit
   
   module Rack
   
     class Simulator
-  
-      require 'uuid'
-      require 'haml'
-      require 'yaml'
-      require 'time'
-      require 'rack/logger'
       
-      ## Default record filter mixin code
-      require 'shibkit/rack/simulator/record_filter'
+      ## Methods have been split up into mixins to make things more manageable
+      include Shibkit::Rack::Simulator::Mixin::Injection
+      include Shibkit::Rack::Simulator::Mixin::Render
+      include Shibkit::Rack::Simulator::Mixin::Actions
+      include Shibkit::Rack::Simulator::Mixin::Logging
       
       ## Easy access to Shibkit's configuration settings
       include Shibkit::Configured
     
       ## Middleware application components and behaviour
       CONTENT_TYPE   = { "Content-Type" => "text/html; charset=utf-8" }
-      VIEWS          = [:user_chooser, :fatal_error]
+      VIEWS          = [:user_chooser, :fatal_error, :session_status]
   
       def initialize(app)
       
         ## Rack app
         @app = app
-
-        ## Initial vars for storing cached data
-        @views    = nil
-        @users    = nil
-        @orgtree  = nil 
-      
-        ## Can Chooser IDPs have Single Sign On? # TODO needs per-IDP settings too
-        @sso = config.sim_chooser_idp_sso
-        
-        ## Add the record processing mixin if it's present
-        load_filter_mixin
-      
-        ## Load and cache the data sources for users and chooser organisations
-        user_data
-      
-        ## Load the Federation and org/IDP data
-        # ...
-      
-        ## Check that everything is OK
-        check_state # TODO: check for working session
-      
+  
       end
   
       ## Selecting an action and returning to the Rack stack 
@@ -54,24 +47,115 @@ module Shibkit
       
         ## Peek at user input, they might be talking to us
         request = ::Rack::Request.new(env)
-      
+        
+        ## Models used to adjust session states
+        idp  = Shibkit::Rack::Simulator::Model::IDPSession.new(env)
+        sp   = Shibkit::Rack::Simulator::Model::SPSession.new(env)
+        wayf = Shibkit::Rack::Simulator::Model::WAYFSession.new(env)
+        dir  = Shibkit::Rack::Simulator::Model::IDP_Session.new(env)
+        
+        models = {:idp => idp, :sp => sp, :wayf => wayf, :dir => dir}
+        
+        ## Catching exceptions in the workflow/routing
         begin
 
-          ## First act according the the URL path selected by user
+          ## Route to actions according to requested URLs
           case request.path
           
-          ## Request is for the fake IDP's login function
-          when sim_idp_login_path
-          
-            ## Where do we send users to after they authenticate with IDP?
-            destination = request.params['destination'] || '/'
-          
-            ## Specified a user? (GET or POST)
-            user_id = request.params['user']
+          ## IDP status information
+          when idp.status_path
             
-            if user_id 
+            return idp_status_action(env, models)
+            
+          ## IDP session information
+          when idp.session_path
+            
+            return idp_session_action(env, models) 
+          
+          ## Request is for the fake IDP's login function
+          when idp.login_path
+          
+            ## Specified a user? (GET or POST) then try logging in
+            if request.params['user'] 
+              
+              return idp_login_action(env, models)
+            
+            ## Already logged in? With SSO log in again.
+            elsif idp.sso? and idp.logged_in?
+              
+              return idp_sso_action(env, models)
+            
+            ## Show the chooser page to present login options  
+            else
+            
+              return idp_simple_chooser_action(env, models)
+              
+          ## IDP SLO request?     
+          when idp.logout_path
+              
+            return idp_logout_action(env, models)
+            
+          ## WAYF request?
+          when wayf.path
+              
+            return wayf_action(env, models)  
+            
+          ## SP session status page?
+          when sp.session_path
+              
+            return sp_session_status_action(env, models)
+          
+          ## SP protected page?    
+          when sp.masked_path
+            
+            ## Valid session in SP
+            if sp.logged_in?
+              
+              return sp_protected_page_action(env, models)
+              
+            else
+              
+              return sp_login_action(env, models)
+              
+            end
+            
+          else
+            
+            ## Do nothing, pass on up to the application
+            return @app.call(env, models)
+            
+        end
+
+        ## Catch any errors generated by this middleware class. Do not catch other Middleware errors.
+        rescue Rack::Simulator::RuntimeError => oops
+        
+          ## Render a halt page
+          return fatal_error_action(env, oops)
+    
+        end
+
+      end
+  
+      private
+  
+      # ...
+  
+    end
+
+  end
+end
+
+ 
+            
+ =================================
+              
+               ## Where do we send users to after they authenticate with IDP?
+                destination = request.params['destination'] || '/'
               
               log_debug("(IDP) New user authentication requested")
+              
+                      
+                        
               
               ## Get our user information using the param
               user_details = users[user_id.to_s]
@@ -205,7 +289,12 @@ module Shibkit
             ## Redirect to fake IDP URL (or wayf, later)
             return [ 302, {'Location'=> "#{sim_idp_login_path}?destination=#{destination}" }, [] ]
 
-
+          when sim_sp_session_path
+            
+            log_debug("(SP) Showing SP session status page")
+          
+            return sp_session_action(env, stats)
+            
           ## If not a special or authenticated URL
           else
          
@@ -216,423 +305,5 @@ module Shibkit
            return @app.call(env)
          
           end
-        
-        ## Catch any errors generated by this middleware class. Do not catch other Middleware errors.
-        rescue Rack::Simulator::RuntimeError => oops
-        
-          ## Render a halt page
-          return fatal_error_action(env, oops)
-    
-        end
 
-      end
-  
-      private
-  
-      ## Wipe clean the Rack session info for this middleware (SP and IDP)
-      def reset_sessions(env)
 
-       sim_sp_session(env).replace  Hash.new
-       sim_idp_session(env).replace Hash.new
-       
-       tidy_request(env)
-    
-      end
-      
-      ## Remove all injected headers
-      def flush_headers(env)
-      
-        # TODO
-        # ...
-      
-      end
-      
-      ## Error page for unrecoverable situations
-      def fatal_error_action(env, oops)
-      
-        log_debug("****  Fatal error: #{oops}")
-      
-        unless ENV['RACK_ENV'] == :production or ENV['RAILS_ENV'] == :production
-
-          puts "\nBacktrace is:\n#{oops.backtrace.to_yaml}\n"
-
-        end
-    
-        render_locals = { :message => oops.to_s }
-        page_body = render_page(:fatal_error, render_locals)
-    
-        return 500, CONTENT_TYPE, [page_body.to_s]
-    
-      end
-  
-      ## Controller for user presentation page
-      def user_chooser_action(env, options={}) 
-    
-         message = options[:message] 
-         code    = options[:code].to_i || 200
-    
-         render_locals = { :organisations => organisations, :users => users,
-                           :message => message, :idp_path => sim_idp_login_path }
-         page_body = render_page(:user_chooser, render_locals)
-       
-         return code, CONTENT_TYPE, [page_body.to_s]
-    
-      end
-  
-      ## Display a chooser page
-      def render_page(view, locals={})
-    
-        ## HAML rendering options
-        Haml::Template.options[:format] = :html5
-    
-        ## Render and return the page
-        haml = Haml::Engine.new(views[view])
-        return haml.render(Object.new, locals)
-    
-      end
-  
-      ## Add attribute information to the headers passed to application
-      def inject_attribute_headers(env, user_details)
-      
-        ## The ID of this simulated SP # TODO: needs to be defined in config
-        sp_id = config.sim_sp_entity_id
-      
-        ## Convert to proper format that matches the live SP (also add new ones)
-        prepared_data = process_attribute_data(user_details)
-    
-        ## Now the useful bit
-        prepared_data.each_pair do | header, value| 
-      
-          env[header] = value
-      
-        end
-      
-        ## Inject the rather important eptid varieties
-        env['targeted-id']   = prepared_data['targeted_id']   || 
-          Shibkit::DataTools.targeted_id(user_details['id'], sp_id, user_details['idp_scope'], user_details['idp_salt'], type=:computed)
-        env['persistent-id'] = prepared_data['persistent_id'] ||
-          Shibkit::DataTools.persistent_id(user_details['id'], sp_id, user_details['idp_id'], user_details['idp_salt'], type=:computed)
-        
-        ## Cache the persistent ID for this user
-        user_details['persistent_id'] = env['persistent-id']
- 
-      end
-
-      ## Add attribute information to the headers passed to application
-      def inject_session_headers(env, user_details)
-      
-        ## The ID of this simulated SP # TODO: needs to be defined in config
-        sp_id = config.sim_sp_entity_id
-      
-        ## Application ID
-        env['Shib-Application-ID'] = 'default'
-    
-        ## Persistent Session ID
-        session_id = sim_sp_session(env)[:sessionid]
-        env['Shib-Session-ID'] = session_id
-    
-        ## Identity Provider ID
-        env['Shib-Identity-Provider'] = sp_id
-    
-        ## Time authentication occured
-        env['Shib-Authentication-Instant'] = sim_sp_session(env)[:logintime]
-    
-        ## Keep login method rather vague
-        env['Shib-Authentication-Method'] = 'urn:oasis:names:tc:SAML:1.0:am:unspecified'
-        env['Shib-AuthnContext-Class']    = 'urn:oasis:names:tc:SAML:1.0:am:unspecified'
-    
-        ## Assertion headers are cargo-culted for not (not sensible - Do Not Use)
-        assertion_header_info(session_id, user_details).each_pair {|header, value| env[header] = value}
-        env['Shib-Assertion-Count'] = "%02d" % assertion_header_info(session_id, user_details).size
-    
-        ## Is targeted ID set to be automatic?
-        env['REMOTE_USER'] = user_details['persistent_id'] ||
-          Shibkit::DataTools.persistent_id(user_details['id'], sp_id, user_details['idp_id'], user_details['idp_salt'], type=:computed)
-     
-      end
-  
-      ## Munge the data in attributes to match Shib/SAML expectations
-      def process_attribute_data(user_details)
-    
-        munged_data = user_details.dup
-    
-        ## Call out to filter (this is monkey patched by shibsim_filter.rb)
-        munged_data = user_record_filter(munged_data)
-          
-        return munged_data
-    
-      end
-    
-      ## User-overridable method - monkey patch with shibsim_filter.rb
-      def user_record_filter(munged_data)
-      
-        return munged_data
-      
-      end
-    
-  
-      ## Create information for mocking assertion headers
-      def assertion_header_info(session_id, user_details) 
-    
-        info = Hash.new
-      
-        ## We need this again in order to calculate accurate assertion size
-        munged_data = process_attribute_data(user_details)
-      
-        ## This should be based on total size of assertion data I believe (this is Shib1.3 style?)
-        (1..2).each do |assertion_part|
-      
-          ## Each assertion fragment gets a numbered identifier
-          header = 'Shib-Assertion-' + "%02d" % assertion_part
-      
-          ## Building up a mock URL
-          value  = config.sim_assertion_base + '?key=' + session_id + '&ID=' + Shibkit::DataTools.xsid
-    
-          ## Collect it
-          info[header] = value
-      
-        end
-    
-        return info
-  
-      end
-  
-      ## Create the SP/IDP basic session
-      def set_session(env, user_details)
-        
-        ## Keep the user ID so we can reapply attributes in the future
-        sim_idp_session(env)[:user_id] = user_details['id']
-      
-        ## Contruct a session ID is if we don't have one
-        sim_sp_session(env)[:sessionid] = Shibkit::DataTools.xsid
-    
-        ## Store login time as a string in xs:DateTime format (with no timezone for some reason)
-        sim_sp_session(env)[:logintime] = Time.new.xmlschema.gsub(/(\+.*)/, 'Z')
-
-    
-      end
-      
-      ## Access simulated IDP session information
-      def sim_idp_session(env)
-        
-        ## Make sure we have a data structure
-        env['rack.session']['shibkit-simulator']        ||= Hash.new
-        env['rack.session']['shibkit-simulator']['idp'] ||= Hash.new
-        
-        return env['rack.session']['shibkit-simulator']['idp']
-        
-      end
-      
-      ## Access simulated SP session information
-      def sim_sp_session(env)
-   
-        ## Make sure we have a data structure
-        env['rack.session']['shibkit-simulator']        ||= Hash.new
-        env['rack.session']['shibkit-simulator']['sp'] ||= Hash.new
-       
-        return env['rack.session']['shibkit-simulator']['sp']
-        
-      end
-      
-      ## Inject headers into session as if provided by a real SP
-      def inject_sp_headers(env, user_details)
-        
-        raise Rack::Simulator::RuntimeError, "Missing user details when trying to add SP headers" unless user_details
-        
-        ## Inject data into the headers that application will receive
-        inject_attribute_headers(env, user_details)
-    
-        ## Fake various Shibboleth headers that are session-specific
-        inject_session_headers(env, user_details)
-    
-      end
-
-      ## Remove ShibSimulator params, etc from request before it reaches application
-      def tidy_request(env)
-    
-        #req = Rack::Request.new(env)
-    
-        #[:shibsim_user, :shibsim_reset].each do |param|
-    
-        #  req.params.delete(param.to_s)
-    
-       # end
-    
-      end
-  
-      ## List user records by ID
-      def users
-    
-        return user_data[0]
-  
-      end
-  
-      ## List user records by ID
-      def organisations
-  
-        return user_data[1]
-  
-      end
-
-      ## Load and prepare HAML views
-      def views
-    
-        unless @views
-    
-          @views = Hash.new
-    
-          VIEWS.each do |view| 
-
-            view_file_location = "#{::File.dirname(__FILE__)}/simulator/views/#{view.to_s}.haml"
-            @views[view] = IO.read(view_file_location)
-
-          end
-    
-        end
-    
-        return @views
-    
-      end
-  
-      ## Provide user data for chooser and header injection
-      def user_data
-    
-        unless @users && @orgtree
-    
-          @users   = Hash.new
-          @orgtree = Hash.new 
-      
-          user_fixture_file_location = "#{::File.dirname(__FILE__)}/simulator/default_data/users.yml"
-
-          fixture_data = YAML.load_file(config.sim_users_file )
-
-          fixture_data.each_pair do |label, record| 
- 
-            record['shibsim_label'] = label.to_s.strip
-            rid  = record['id'].to_s
-            rorg = record['organisation'].to_s.strip
-          
-            ## Salt to use is based on org name
-            record['idp_salt'] = Digest::SHA1.digest(rorg).chomp # TODO: make configurable
-          
-            @users[rid]    =   record        
-            @orgtree[rorg] ||= Array.new
-        
-            @orgtree[rorg] <<  record 
-        
-          end
- 
-        end
-
-        return [@users, @orgtree]
-    
-      end
-    
-      ## Add the filter mixin if it exists
-      def load_filter_mixin
-      
-        eval "extend #{config.sim_record_filter_module}"
-      
-      end
-    
-      def check_state
-      
-        raise "No user data!" unless @users.size > 0 
-        raise "No organisation labels!" unless @orgtree.size > 0
-      
-      end
-    
-      ## Simple and switchable logger (to stdout by default)
-      def log_debug(message)
-        
-        return unless config.sim_debug
-        
-        puts [Time.new, "Shibkit-Simulator:", message].join(' ')
-      
-      end
-
-      ## Does an 'IDP' session exist already?
-      def existing_idp_session?(env)
-      
-        ## Look for evidence of existing session: Has the fake IDP 'authenticated'?
-        return true if env["rack.session"] and
-          env["rack.session"]['shibkit-simulator'] and
-          env["rack.session"]['shibkit-simulator']['idp'] and
-          env["rack.session"]['shibkit-simulator']['idp'][:user_id] and 
-          env["rack.session"]['shibkit-simulator']['idp'][:user_id].to_i > 0
-        
-        
-        
-        # ...
-        
-        return false
-      
-      end
-    
-      ## Does an 'SP' session exist already?
-      def existing_sp_session?(env)
-    
-        ## Has the fake IDP 'authenticated'?
-        return false unless env["rack.session"] and
-          env["rack.session"]['shibkit-simulator'] and
-          env["rack.session"]['shibkit-simulator']['sp'] and
-          env["rack.session"]['shibkit-simulator']['sp'][:user_id] and 
-          env["rack.session"]['shibkit-simulator']['sp'][:user_id].to_i > 0
-        
-        # ...
-        
-        ## Check that the *same* user has already authenticated with the fake SP too.
-        return true if env["rack.session"]['shibkit-simulator']['idp'][:user_id].to_i == 
-          sim_sp_session(env)[:user_id].to_i
-        
-        return false
-      
-      end
-    
-      ## Is the requested user valid?
-      def user_details_ok?(user_details)
-      
-        return true if user_details and user_details.kind_of?(Hash) and
-            user_details.size > 1 
-      
-        return false
-      
-      end
-      
-      ## Define various URL matchers
-      def sim_idp_login_path
-      
-        return "/shibsim_idp/login"
-      
-      end
-    
-      def sim_idp_logout_path
-      
-        return "/shibsim_idp/logout"
-      
-      end
-
-      def sim_wayf_path
-      
-        return "/shibsim_wayf"
-    
-      end
-
-      def sim_sp_path_regex
-      
-        regex = /.*/
-        
-        return regex
-      
-    
-      end
-
-      ## Exception class used here to limit rescued errors to this middleware only
-      class Rack::Simulator::RuntimeError < Shibkit::RackMiddlewareError 
-    
-      end
-    
-    end
-
-  end
-end
