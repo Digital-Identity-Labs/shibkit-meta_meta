f2434f1a6b0b8e8e56da4d738f5f9487d969d408
Pete Birkinshaw <pete@binary-ape.org>
2011-02-09 22:44:19 +0000

    BROKEN (LOCAL) COMMIT: mid-way through refactoring two middlware (Simulator and Demo) into four middleware...

---
+++ shibkit/rack/simulator/mixins/dir_actions.rb
@@ -0,0 +1,57 @@
+module Shibkit
+  module Rack
+    class Simulator
+      module Mixin
+        module DirActions
+              
+                       
+          ####################################################################
+          ## Directory Actions
+          ##
+          
+          ## Controller for directory Search/List 
+          def directory_list_action(env, directory, options={})
+            
+            #message = options[:message] || idp_session.get_message
+
+             code = 200
+
+             locals = get_locals(
+               :layout     => :layout,
+               :directory  => directory,
+               :page_title => directory.display_name,
+               :message    => "message"
+               ) 
+
+             page_body = render_page(:directory_list, locals)
+
+             return code, CONTENT_TYPE, [page_body.to_s]
+                
+          end
+
+          ## Controller for directory entry 
+          def directory_item_action(env, directory, options={})
+            
+            #message = options[:message] || idp_session.get_message
+
+             code = 200
+
+             locals = get_locals(
+               :layout     => :layout,
+               :directory  => directory,
+               :user       => options[:account],
+               :page_title => directory.display_name,
+               :message    => "message"
+               ) 
+
+             page_body = render_page(:directory_item, locals)
+
+             return code, CONTENT_TYPE, [page_body.to_s]
+                
+          end
+          
+        end
+      end
+    end
+  end
+end
---
+++ shibkit/rack/simulator/mixins/ds_actions.rb
@@ -0,0 +1,71 @@
+module Shibkit
+  module Rack
+    class Simulator
+      module Mixin
+        module DSActions
+          
+
+          ####################################################################
+          ## WAYF Actions
+          ##
+          
+          ## Controller for
+          def wayf_action(env, wayf_session, options={})
+            
+            request = ::Rack::Request.new(env)
+            term = request.params['term'].to_s.downcase.strip[0..40]
+            idps = Shibkit::Rack::Simulator::Model::IDPService.all
+            
+            code = 200
+              
+            ## We've got a term request so respond with JSON
+            unless term.empty?  
+              
+              results = Array.new
+              
+              idps.each do |idp|
+              
+                if idp.display_name.downcase =~ /#{term}/  
+              
+                  result = Hash.new
+                  result['id']    = idp.id
+                  result['label'] = idp.display_name
+                  result['value'] = idp.display_name
+                 
+                  results << result
+                  
+                end
+              
+              end
+              
+              page_body = results.to_json
+              
+              puts page_body
+              
+              return code,
+                { "Content-Type" => "application/json" },
+                page_body
+              
+            end  
+                                   
+            locals = get_locals(
+              :layout     => :layout,
+              :javascript => :wayf,
+              :wayf       => wayf_session, # TODO: should this be the service instead?
+              :idps       => idps.sort! { |a,b| a.display_name.downcase <=> b.display_name.downcase },
+              :page_title => "Select Your Home Organisation"
+            )
+            
+            page_body = render_page(:wayf_smart, locals)
+              
+            return code, CONTENT_TYPE, [page_body.to_s]
+                
+          end
+          
+         
+
+        end
+      end
+    end
+  end
+end
