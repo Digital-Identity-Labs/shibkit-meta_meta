47f2ff1288dd080c9225b7ce7f4850ed510c76a0
Pete Birkinshaw <pete@binary-ape.org>
2011-06-27 11:10:44 +0100

    Tweaked Gemfile and added new copyright/license info (going from MIT to Apache2)

--- lib/shibkit/metameta.rb
+++ lib/shibkit/metameta.rb
@@ -1,14 +1,24 @@
-## This file is part of Shibkit
+## @author    Pete Birkinshaw (<pete@digitalidentitylabs.com>)
+## Copyright: Copyright (c) 2011 Digital Identity Ltd.
+## License:   Apache License, Version 2.0
+
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
 ##
-## Author::    Pete Birkinshaw  (mailto:pete@binary-ape.org)
-## Copyright:: Copyright (c) 2010 Pete Birkinshaw & Digital Identity Ltd
-## License::   MIT (Please read the LICENSE file shipped with this code)
 
 require 'rubygems'
 require 'nokogiri'
 require 'yaml'
 
-require 'shibkit'
 require 'shibkit/metameta/contact'
 require 'shibkit/metameta/source'
 require 'shibkit/metameta/entity'
